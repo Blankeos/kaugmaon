bc1e0007b312c52237496b41a046f7e5cdf860f8
Blankeos <carloantonioct@gmail.com>
2023-01-29 21:22:09 +0800

    Added auth and working ticket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,12 @@ import Link from "next/link";
 import "./globals.css";
 import styles from "./page.module.css";
 import ClientToaster from "@/components/ClientToaster";
+
+import AuthContext from "@/context/AuthContext";
+
 export default function RootLayout({
   children,
+  ...props
 }: {
   children: React.ReactNode;
 }) {
@@ -16,6 +20,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
+        {/* <AuthContext> */}
         <nav className="fixed top-0 w-full z-20 backdrop-blur-sm">
           <div className="fluid-container">
             {/* Nav Content Start */}
@@ -48,33 +53,11 @@ export default function RootLayout({
           </div>
         </nav>
         <div className="min-h-screen flex flex-col bg-dark">
-          {/* <nav className={styles.description}>
-            <p>
-              Get started by editing&nbsp;
-              <code className={styles.code}>app/page.tsx</code>
-            </p>
-            <div>
-              <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                By{" "}
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
-                  width={100}
-                  height={24}
-                  priority
-                />
-              </a>
-            </div>
-          </nav> */}
           <main className="flex flex-grow flex-col">{children}</main>
           <footer className="fluid-container">Footer</footer>
         </div>
         <ClientToaster />
+        {/* </AuthContext> */}
       </body>
     </html>
   );
