1ad876b247ed5ee066a2c68cb6141f86e8329fe8
Blankeos <carloantonioct@gmail.com>
2023-04-13 19:51:39 +0800

    #5 content: the reels and location eventdata

--- src/data/eventData.ts
+++ src/data/eventData.ts
@@ -1,7 +1,7 @@
 const eventData = {
   time: "7:30 AM",
   date: "April 14, 2023",
-  location: "College of Medicine Multipurpose Covered Gym",
+  location: "College of Medicine Gym",
 };
 
 export default eventData;
